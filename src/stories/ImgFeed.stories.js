f725fe479da1db8a479de159d5c96169083388bf
hdorothea <dorothea.hormann@gmail.com>
2017-10-04 16:29:54 -0400

    Add ImgFeed Component

---
+++ src/stories/ImgFeed.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import ImgFeed from '../components/ImgFeed';
+
+export default storiesOf('ImgFeed', module).add('renders', () => (
+  <ImgFeed
+    imgSrcs={[
+      'https://d3nevzfk7ii3be.cloudfront.net/igi/NU5mrvIoyFDFuSFH',
+      'https://d3nevzfk7ii3be.cloudfront.net/igi/baGSWoqAItOfDGuk',
+    ]}
+    imgDescriptions={['descriptive Image', 'descriptive Image']}
+  />
+));
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import FeedStories from './Feed.stories';
+// eslint-disable-next-line no-unused-vars
+import ImgFeedStories from './ImgFeed.stories';
 
 // storiesOf('Button', module)
 //   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
