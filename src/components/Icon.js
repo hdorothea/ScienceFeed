89ffe270709b16fda4aa9ae89917d1bd82fdc1e4
hdorothea <dorothea.hormann@gmail.com>
2017-10-13 14:58:23 -0400

    Switch to svg icons

---
+++ src/components/Icon.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Icon(props) {
+  const styles = {
+    svg: {
+      display: 'inline-block',
+      verticalAlign: 'middle',
+    },
+    path: {
+      fill: props.color,
+    },
+  };
+
+  return (
+    <svg
+      style={styles.svg}
+      width={`${props.size}px`}
+      height={`${props.size}px`}
+      viewBox="0 0 1024 1024"
+    >
+      {props.paths.map(path => <path style={styles.path} d={path} />)}
+    </svg>
+  );
+}
+
+Icon.propTypes = {
+  paths: PropTypes.array.isRequired,
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  size: 16,
+  color: 'black',
+};
